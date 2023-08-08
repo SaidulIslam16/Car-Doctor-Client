6d68ff78ee56e4aecf7ac629445b3e88099fdc22
SaidulIslam16 <isaidulislamsayed@gmail.com>
2023-08-08 18:50:09 +0600

    carousel added

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -4,9 +4,19 @@ import img3 from "../../../assets/images/banner/3.jpg"
 import img4 from "../../../assets/images/banner/4.jpg"
 const Banner = () => {
     return (
-        <div className="carousel w-full">
+        <div className="carousel w-full h-[600px]">
             <div id="slide1" className="carousel-item relative w-full">
                 <img src={img1} className="w-full" />
+                <div className="absolute text-white flex items-center w-1/2 gap-4 top-0  left-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)] h-full ">
+                    <div className="pl-20 space-y-5">
+                        <h2 className="text-7xl font-bold">Affordable Price For Car Servicing</h2>
+                        <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                        <div>
+                            <button className="btn btn-active btn-primary mr-5">Discover More</button>
+                            <button className="btn btn-outline btn-primary">Latest Projects</button>
+                        </div>
+                    </div>
+                </div>
                 <div className="absolute flex justify-end gap-4 transform -translate-y-1/2 left-5 right-5 bottom-0">
                     <a href="#slide4" className="btn btn-circle">❮</a>
                     <a href="#slide2" className="btn btn-circle">❯</a>
